17bc0fe48a852cdc9900d8d06b09030c4f2d15bc
Yujin Chung <yujinchung@Yujinui-MacBookAir.local>
2024-11-05 16:37:34 +0900

    initial commit

---
+++ bin/battle_rpg_game.dart
@@ -0,0 +1,5 @@
+import 'package:battle_rpg_game/battle_rpg_game.dart' as battle_rpg_game;
+
+void main(List<String> arguments) {
+  print('Hello world: ${battle_rpg_game.calculate()}!');
+}
---
+++ test/battle_rpg_game_test.dart
@@ -0,0 +1,8 @@
+import 'package:battle_rpg_game/battle_rpg_game.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
